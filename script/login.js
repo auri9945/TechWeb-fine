6df73f8a5be27859c1dfb2601143efb24b3b2a60
stefyscap <scarpati.stefania98@gmail.com>
2024-01-21 11:19:51 +0100

    css

--- script/login.js
+++ script/login.js
@@ -14,5 +14,36 @@ $(document).ready(function(){
       $("#myModal").find("#password").val('');
     });
     // FINE SCRIPT POP UP LOGIN
-    
+     
+    $(document).on('submit', '#loginForm', function(e) {
+      // evito che il submit ricarichi la pagina
+      e.preventDefault();
+
+      // costruisco un oggetto con i campi del popup#popupCls_signUp
+      var loginObj = {};
+      loginObj.email = $("#username").val();
+      loginObj.password = $("#password").val().trim();
+
+      // chiamata per l'inserimento a DB
+      $.ajax({
+          url: 'api_server/loginSystem.php',
+          dataType: "json",
+          type: 'POST',
+          data: {login: loginObj},
+          success: function(data) {
+              console.log(data.message);
+              $("#popupCls").click();
+              loadPosts();
+              alert(data.message);
+          },
+          error: function(data) {
+              console.log(data);
+              alert("Errore in fase di login. Email o Password errata");
+          }
+      });
+    });
+
+
+
+
 });
